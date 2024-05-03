81898344c8588e6aa42e1e77fc1926ef20844e9d
HeseltineBTutu <heseltinetutu@gmail.com>
2024-05-03 09:27:37 -0700

    added not found error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,29 @@
 """
 Module for running Flask application.
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+# Handler for 404 errors
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handle 404 errors by returning a JSON-formatted response.
+
+    Args:
+        error (Exception): The exception object representing the 404 error.
+
+    Returns:
+        tuple: A tuple containing the JSON-formatted response and the HTTP
+        status code (404).
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
